31779b33f1b51bc56c98a7763cc6abf82cdd673b
戴宙峰 <daizhoufeng@mininglamp.com>
2020-04-23 13:29:03 +0800

    修复unicode的bug

--- week02/str2utf8.js
+++ week02/str2utf8.js
@@ -5,31 +5,37 @@ function str2utf8(str){
 
         const nums2 = str.charCodeAt(i).toString(2)
         const nums16 = str.charCodeAt(i).toString(16)
+        console.log('nums2:',nums2)
+        console.log('nums16:',nums16)
         const numsCeil = getLen(nums16) // 该数需要在多少个字节
+        console.log('numsCeil:',numsCeil)
         if(numsCeil === 1){
             arr.push('0' + nums2)
         }else{
             let k = 0;
             const nums2Arr = nums2.split('')
-            let nums2utf = ''
             let nums = nums2Arr.pop()
+            let nums2utf = ''
+            console.log('nums2Arr:',nums2Arr)
             while(typeof nums !== 'undefined'){
-                if((nums2utf.length + numsCeil + 2) % 8 === 0 && k === 0){
+                console.log('arr:',arr)
+                console.log('nums2utf:',nums2utf)
+                if((nums2utf.length + numsCeil + 2) % 8 === 0 && k === numsCeil){
                     nums2utf = getNums('1',numsCeil)+'0'+ nums + nums2utf
-                    arr[i]+=nums2utf
+                    arr[i] = nums2utf + arr[i]
                     nums2utf = ''
                     k ++ 
-                }else if((nums2utf.length + 3) % 8 === 0 && k > 0) {
+                }else if((nums2utf.length + 3) % 8 === 0 && k !== numsCeil) {
                     nums2utf = '10'+ nums + nums2utf
-                    arr[i]+=nums2utf
+                    arr[i] = nums2utf + arr[i]
                     nums2utf = ''
                     k ++ 
                 }else {
                     nums2utf = nums + nums2utf
                 }
                 if(nums2Arr.length === 0 && nums2utf.length < numsCeil * 8){
-                    nums2utf = '10'+ getNums('0',6 - nums2utf.length) + nums2utf
-                    arr[i]+=nums2utf
+                    nums2utf = getNums('1',numsCeil) + '0' + getNums('0',4 - nums2utf.length) + nums2utf
+                    arr[i] = nums2utf + arr[i]
                     nums2utf = ''
                     k ++ 
                 }
